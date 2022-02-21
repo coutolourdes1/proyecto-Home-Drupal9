121bf7836b4f0a4172c71ec4746c1c7263b00921
coutolourdes1 <coutolourdes2020@gmail.com>
2022-02-21 12:26:02 -0300

    contiene carrusel, buscador y card con noticias

---
+++ web/sites/default/files/js/js_wrkKCaghrJ7ZyhG5ztHiIwHApPirBeRebR6IKJ9GHFI.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.subtema_nro1 = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
+;
